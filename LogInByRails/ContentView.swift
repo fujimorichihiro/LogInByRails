a8933a067ff9c910935c6789dc6ce790b15b6cac
fujimorichihiro <fujimori.drum@gmail.com>
2021-03-07 20:28:02 +0900

    Initial Commit

---
+++ LogInByRails/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LogInByRails
+//
+//  Created by 藤森千裕 on 2021/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LogInByRails/LogInByRailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogInByRailsApp.swift
+//  LogInByRails
+//
+//  Created by 藤森千裕 on 2021/03/07.
+//
+
+import SwiftUI
+
+@main
+struct LogInByRailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
